236076222bd74277b96243a276024cf48c169401
kaka <murira.evans@gmail.com>
2019-01-13 23:35:02 +0300

    make the type object

--- schema/schema.js
+++ schema/schema.js
@@ -1,1 +1,21 @@
+const graphql = require('graphql');
+const axios = require('axios');
 
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id:{type:GraphQLInt},
+    first_name:{type: GraphQLString},
+    last_name:{type:GraphQLString},
+    email:{type:GraphQLString},
+    gender:{type:GraphQLString}
+  })
+})
